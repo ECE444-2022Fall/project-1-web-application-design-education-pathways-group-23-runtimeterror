3eec364c449242d8228890122f41777a5c18cfac
deandyu <deand.yu@mail.utoronto.ca>
2022-11-12 18:27:14 -0500

    Add backend support for theme searches

--- Education_Pathways/tests/test_search.py
+++ Education_Pathways/tests/test_search.py
@@ -18,10 +18,20 @@ def test_search_name(initialize_db):
 
 def test_search_name_minor(initialize_db):
     # Course name, minor
-    assert search_course("Algorithms and Data Structures", "AEMINAIEN")[0]["code"] == "ECE345H1", \
+    assert search_course("Algorithms and Data Structures", minor="AEMINAIEN")[0]["code"] == "ECE345H1", \
         "Incorrect course found or course not found"
 
 def test_search_code_minor(initialize_db):
     # Course code, minor
-    assert search_course("ECE345", "AEMINAIEN")[0]["code"] == "ECE345H1", \
+    assert search_course("ECE345", minor="AEMINAIEN")[0]["code"] == "ECE345H1", \
+        "Incorrect course found or course not found"
+
+def test_search_name_theme(initialize_db):
+    # Course name, theme
+    assert search_course("Physiological Control Systems", mse_theme="Biomaterials")[0]["code"] == "BME331H1", \
+        "Incorrect course found or course not found"
+
+def test_search_code_theme(initialize_db):
+    # Course code, theme
+    assert search_course("BME331H1", mse_theme="Biomaterials")[0]["code"] == "BME331H1", \
         "Incorrect course found or course not found"
