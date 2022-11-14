a682fd2fcf4882cba72c8616cf9866d58061b34e
Valentina Manferrari <valentina.manferrari@gmail.com>
2022-11-14 17:20:19 -0500

    First prototype of course description legend

--- Education_Pathways/frontend/src/components/CourseDescription.js
+++ Education_Pathways/frontend/src/components/CourseDescription.js
@@ -13,7 +13,7 @@ let star = empty_star;
 
 class CourseDescriptionPage extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props)
 
     this.state = {
@@ -21,7 +21,7 @@ class CourseDescriptionPage extends Component {
       course_name: "",
       division: "Faculty of Applied Science and Engineering",
       department: "Department of Edward S. Rogers Sr. Dept. of Electrical & Computer Engineering",
-      graph : "",
+      graph: "",
       course_description: "",
       syllabus: "",
       prerequisites: "",
@@ -40,35 +40,35 @@ class CourseDescriptionPage extends Component {
     })
       .then(res => {
         console.log(res.data.course)
-        this.setState({course_code: res.data.course.code})
-        this.setState({course_name: res.data.course.name})
-        this.setState({course_description : res.data.course.description})
-        this.setState({graph: res.data.course.graph})
+        this.setState({ course_code: res.data.course.code })
+        this.setState({ course_name: res.data.course.name })
+        this.setState({ course_description: res.data.course.description })
+        this.setState({ graph: res.data.course.graph })
         let prereq_len = res.data.course.prereq.length
         if (prereq_len > 1) {
           let prereq_str = ""
           for (let i = 0; i < prereq_len; i++) {
-              prereq_str += res.data.course.prereq[i]
-              if (i !== prereq_len - 1) {
-                prereq_str += ", "
-              }
+            prereq_str += res.data.course.prereq[i]
+            if (i !== prereq_len - 1) {
+              prereq_str += ", "
+            }
           }
-          this.setState({prerequisites : prereq_str})
+          this.setState({ prerequisites: prereq_str })
         } else {
-          this.setState({prerequisites : res.data.course.prereq})
+          this.setState({ prerequisites: res.data.course.prereq })
         }
         let takenreq_len = res.data.course.takenreq.length
         if (takenreq_len > 1) {
           let takenreq_str = ""
           for (let i = 0; i < takenreq_len; i++) {
             takenreq_str += res.data.course.takenreq[i]
-              if (i !== takenreq_len - 1) {
-                takenreq_str += ", "
-              }
+            if (i !== takenreq_len - 1) {
+              takenreq_str += ", "
+            }
           }
-          this.setState({prerequisitestaken : takenreq_str})
+          this.setState({ prerequisitestaken: takenreq_str })
         } else {
-          this.setState({prerequisitestaken : res.data.course.takenreq})
+          this.setState({ prerequisitestaken: res.data.course.takenreq })
         }
 
 
@@ -81,9 +81,9 @@ class CourseDescriptionPage extends Component {
               coreq_str += ", "
             }
           }
-          this.setState({corequisites : coreq_str})
+          this.setState({ corequisites: coreq_str })
         } else {
-          this.setState({corequisites : res.data.course.coreq})
+          this.setState({ corequisites: res.data.course.coreq })
         }
         let exclusion_len = res.data.course.exclusion.length
         if (exclusion_len > 1) {
@@ -94,22 +94,22 @@ class CourseDescriptionPage extends Component {
               exclusion_str += ", "
             }
           }
-          this.setState({exclusions : exclusion_str})
+          this.setState({ exclusions: exclusion_str })
         } else {
-          this.setState({exclusions : res.data.course.exclusion})
+          this.setState({ exclusions: res.data.course.exclusion })
         }
 
 
         let syllabus_link = "https://github.com/ECE444-2022Fall/project-1-web-application-design-education-pathways-group-23-runtimeterror/tree/develop/Education_Pathways/Syllabi/" + res.data.course.code + ".pdf"
 
-        this.setState({syllabus : syllabus_link})
+        this.setState({ syllabus: syllabus_link })
 
         let temp_graph = []
         //temp_graph.push(<ShowGraph graph_src={this.state.graph}></ShowGraph>)
-        this.setState({graphics: temp_graph})
+        this.setState({ graphics: temp_graph })
 
 
-    })
+      })
 
 
     console.log("new state: ", this.state)
@@ -124,8 +124,8 @@ class CourseDescriptionPage extends Component {
 
   }
 
-	render() {
-		return(
+  render() {
+    return (
 
       <div className="page-content">
         <Container className="course-template">
@@ -160,6 +160,21 @@ class CourseDescriptionPage extends Component {
               <h3>Course Requisites</h3>
             </Row>
             <Row>
+              <div className={"req-graph"}>
+                {/* <img style={{width: "70%", marginBottom: "3%"}} alt="" src={requisite_label}></img>
+                <img src={`data:image/jpeg;base64,${this.state.graph}`} alt="" ></img> */}
+                <div class={'my-legend'}>
+                  <div class={'legend-scale'}>
+                    <ul class={'legend-labels'}>
+                      <li><span class="taken"></span>Course Taken</li>
+                      <li><span class="wishlist"></span>Wishlist</li>
+                      <li><span class="requirement"></span>Requirement</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </Row>
+            <Row>
               <Col className="requisites-display">
                 <h4>Pre-Requisites</h4>
                 <p>{this.state.prerequisites} <span class="taken">{this.state.prerequisitestaken}</span></p>
@@ -173,18 +188,13 @@ class CourseDescriptionPage extends Component {
                 <p>{this.state.exclusions}</p>
               </Col>
             </Row>
-            <Row>
-              <div className={"req-graph"}>
-                <img style={{width: "70%", marginBottom: "3%"}} alt="" src={requisite_label}></img>
-                <img src={`data:image/jpeg;base64,${this.state.graph}`} alt="" ></img>
-              </div>
-            </Row>
+            
           </Row>
         </Container>
       </div>
 
-		)
-	}
+    )
+  }
 }
 
 export default CourseDescriptionPage
