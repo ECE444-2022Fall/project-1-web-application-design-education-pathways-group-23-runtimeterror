58bb4eb3ce847c7daa8f7f47c549fa2906e7198b
Valentina Manferrari <valentina.manferrari@gmail.com>
2022-11-15 01:06:32 -0500

    Added 4 more tests for search API

--- Education_Pathways/tests/test_search_api.py
+++ Education_Pathways/tests/test_search_api.py
@@ -1,7 +1,6 @@
 import pytest
 from flask import session
 from index import create_app
-from course import Course
 
 @pytest.fixture()
 def app():
@@ -23,20 +22,78 @@ def runner(app):
 # Valentina Manferrari
 def test_search_api(client):
     with client:
+        # Given
         input = "ECE444"
         minor = ""
         mse_theme = ""
 
-        response = client.get("/api/search")
-        assert response.status_code == 204
-
+        # Test
         response = client.post("/api/search", json={"input":input, "minor": minor, "mse_theme": mse_theme})
-        response = client.get("/api/search")
         assert response.status_code == 200
-
-        course = Course.deserialize(response.json)
-
-        assert course.name == "Software Engineering"
-        assert course.code == "ECE444H1"
         
+        assert len(response.json) == 1
+        course = response.json[0]
+        assert course['name'] == "Software Engineering"
+        assert course['code'] == "ECE444H1"
 
+def test_search_api_minor(client):
+    with client:
+        # Given
+        input = "ECE344"
+        minor = "AEMINAIEN"
+        mse_theme = ""
+
+        # Test
+        response = client.post("/api/search", json={"input":input, "minor": minor, "mse_theme": mse_theme})
+        assert response.status_code == 200
+        
+        assert len(response.json) == 1
+        course = response.json[0]
+        assert course['name'] == "Operating Systems"
+        assert course['code'] == "ECE344H1"
+
+def test_search_api_mse_theme(client):
+    with client:
+        # Given
+        input = "mse343"
+        minor = ""
+        mse_theme = "Biomaterials"
+
+        # Test
+        response = client.post("/api/search", json={"input":input, "minor": minor, "mse_theme": mse_theme})
+        assert response.status_code == 200
+        
+        assert len(response.json) == 1
+        course = response.json[0]
+        assert course['name'] == "Biomaterials"
+        assert course['code'] == "MSE343H1"
+
+def test_search_api_minor_and_theme(client):
+    with client:
+        # Given
+        input = "mse343"
+        minor = "AEMINBIO"
+        mse_theme = "Biomaterials"
+
+        # Test
+        response = client.post("/api/search", json={"input":input, "minor": minor, "mse_theme": mse_theme})
+        assert response.status_code == 200
+        
+        assert len(response.json) == 1
+        course = response.json[0]
+        assert course['name'] == "Biomaterials"
+        assert course['code'] == "MSE343H1"
+
+def test2_search_api_minor_and_theme(client):
+    with client:
+        # Given
+        input = ""
+        minor = "AEMINBIO"
+        mse_theme = "Biomaterials"
+
+        # Test
+        response = client.post("/api/search", json={"input":input, "minor": minor, "mse_theme": mse_theme})
+        assert response.status_code == 200
+        
+        assert len(response.json) == 5
+    
