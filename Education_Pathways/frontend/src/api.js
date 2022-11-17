11e3eef1818a41ba41a1b915de9b738736c06097
deandyu <deand.yu@mail.utoronto.ca>
2022-11-16 19:46:15 -0500

    Fix axios API url

--- Education_Pathways/frontend/src/api.js
+++ Education_Pathways/frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://dashboard.heroku.com/apps/maple-course-selection/deploy/github",
+  baseURL: "https://maple-course-selection.herokuapp.com/",
   withCredentials: true,
 });
