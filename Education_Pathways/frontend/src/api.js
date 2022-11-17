75fab5127dc59772efc2d0e7043c4e627d8c2481
deandyu <deand.yu@mail.utoronto.ca>
2022-11-16 19:36:11 -0500

    Prepare heroku deployment

--- Education_Pathways/frontend/src/api.js
+++ Education_Pathways/frontend/src/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "https://lab3-docker.herokuapp.com/"
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://dashboard.heroku.com/apps/maple-course-selection/deploy/github",
   withCredentials: true,
 });
